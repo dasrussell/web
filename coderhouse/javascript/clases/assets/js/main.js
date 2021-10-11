6c1d2e6e226064844bc32d9ce3497b763fbca198
dasrussell <dsantamaria0990@gmail.com>
2021-10-11 15:06:40 -0300

    asd

--- coderhouse/javascript/clases/assets/js/main.js
+++ coderhouse/javascript/clases/assets/js/main.js
@@ -223,5 +223,182 @@
 // }
 
 
-    
+/////////////////////////////////// FUNCIONES //////////////////////////////
 
+// function saludar() {
+//     console.log("¡Hola estudiantes!");
+// }
+
+
+// var nombreIngresado   = prompt("Ingresar nombre");
+// alert("El nombre ingresado es " + nombreIngresado);
+// var nombreIngresado   = prompt("Ingresar nombre");
+// alert("El nombre ingresado es " + nombreIngresado);
+// var nombreIngresado   = prompt("Ingresar nombre");
+// alert("El nombre ingresado es " + nombreIngresado);
+
+
+// function solicitarNombre(){
+//     let nombreIngresado   = prompt("Ingresar nombre");
+//     alert("El nombre ingresado es " + nombreIngresado);
+// } 
+
+
+// // PARAMETROS
+// function conParametros(parametro1, parametro2) {
+//     console.log(parametro1 + " " + parametro2)
+// }
+
+// conParametros(6,7)
+
+
+// for (let i = 0; i < 3; i++) {
+//     solicitarNombre();
+// }
+
+
+// function conParametros(parametro1, parametro2) {
+//     console.log(parametro1 + " " + parametro2);
+// }
+
+
+//Declaración de variable para guardar el resultado de la suma
+// let resultado = 0;
+// //Función que suma dos números y asigna a resultado
+// function sumar(primerNumero, segundoNumero) {
+//     resultado = primerNumero + segundoNumero;
+// }
+// //Función que muestra resultado por consola
+// function mostrar(mensaje) {
+//     console.log(mensaje);
+// }
+// //Llamamos primero a sumar y luego a mostrar
+// sumar(6, 3);            
+// mostrar(resultado);
+
+
+// //Otras llamadas a sumar y mostrar
+// sumar(25, 5);            
+// mostrar(resultado); 
+// sumar(1, 2);            
+// mostrar(resultado); 
+
+
+// function sumar(primerNumero, segundoNumero) {
+//     return primerNumero + segundoNumero;
+// }
+// let resultado = sumar(5, 8);
+
+
+// function calculadora(primerNumero, segundoNumero, operacion) {
+//     switch (operacion) {
+//         case "+":
+//             return primerNumero + segundoNumero;
+//             break;
+//         case "-":
+//             return primerNumero - segundoNumero;
+//             break;
+//         case "*":
+//             return primerNumero * segundoNumero;
+//             break;
+//         case "/":
+//             return primerNumero / segundoNumero;
+//             break;
+//         default:
+//             return 0;
+//             break;
+//     }
+// }
+// console.log(calculadora(10, 5, "*"));
+
+
+// let resultado = 0
+// function sumar(primerNumero, segundoNumero) {
+//     resultado = primerNumero + segundoNumero;
+// }
+// sumar(5,6);
+// //Se puede acceder a la variable resultado porque es global
+// console.log(resultado);
+
+
+// function sumar(primerNumero, segundoNumero) {
+//     let resultado = primerNumero + segundoNumero;
+// }
+// //No se puede acceder a la variable resultado fuera del bloque
+// console.log(resultado);
+
+
+// //Generalmente, las funciones anónimas se asignan a variables declaradas como constantes
+// const suma  = function (a, b) { return a + b };
+// const resta = function (a, b) { return a - b };
+// console.log(suma(15,20));
+// console.log(resta(15,5));
+
+
+// const suma  = (a, b) => { return a + b };
+// //Si es una función de una sola línea con retorno podemos evitar escribir el cuerpo.
+// const resta = (a, b) =>  a - b ;
+// console.log(suma(15,20));
+// console.log(resta(20,5));
+
+
+// const suma  = (a,b) => a + b;
+// const resta = (a,b) => a - b;
+// //Si una función es una sola línea con retorno y un parámetro puede evitar escribir los ()
+// const iva   = x => x * 0.21;
+// let precioProducto  = 500; 
+// let precioDescuento = 50;  
+// //Calculo el precioProducto + IVA - precioDescueto
+// let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioDescuento); 
+// console.log(nuevoPrecio);
+
+
+let total = 0;
+
+function agregarAlCarrito() {
+    do {
+        let producto = prompt("¿Quieres comprar shampoo, acondicionador o ambos?")
+        let cantidad = parseInt(prompt("Ingresa la cantidad que deseas del producto"));
+        let precio = 0;
+
+        switch (producto) {
+            case "shampoo":
+                precio = 500;
+                break;
+            case "acondicionador":
+                precio = 700;
+                break;
+            case "ambos":
+                precio = 1100;
+                break;
+            default :
+                alert("Alguno de los ingresados es incorrecto");
+                precio = 0;
+                cantidad = 0;
+        }
+        total = total + precio * cantidad;
+        otroProducto = confirm("¿Quieres agregar otro producto?")
+    }while(otroProducto);
+}
+
+function aplicarDescuento(total) {
+    if(total >= 5000) {
+        total = total * 0.80;
+    }
+    return total;
+}
+function calcularEnvio(total) {
+    let confirmacion = confirm("¿Deseas envío a domicilio?")
+    if(confirmacion && total >= 2000) {
+        alert("Tienes envío gratis. El total de tu compra es $"+total)
+    } else if ( confirmacion && total < 2000 && total != 0) {
+        alert("El envío tiene un de $700. El total de tu compra es $"+total)
+    } else {
+        alert("El total de tu compra es"+total);
+    }
+
+    return total;
+}
+
+agregarAlCarrito();
+calcularEnvio(aplicarDescuento(total));
